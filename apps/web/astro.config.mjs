844473b98e0533569a0852a79dd0eac6801ac047
Sergio Villavicencio <svillavicencio@teco.com.ar>
2026-04-15 04:43:16 -0300

    fix: se cambia adapter para compatibilidad con vercel

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
